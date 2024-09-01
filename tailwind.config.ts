92a247686109d50c3e9785c6a0e3d02e850a9cd1
Zeke Abshire <zekeabshire@gmail.com>
2024-09-01 11:15:35 -0500

    Added shadcn components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
+  darkMode: ["class"],
   content: ["./src/**/*.tsx"],
   theme: {
+    container: { center: true },
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
